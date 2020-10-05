30e222e5c3c2e80765ae1d21100f7d15efcfe78f
Sergey Zhidkov <zhidkovsc@gmail.com>
2020-10-05 21:40:57 +0400

    Add Sidekiq 6.1+ support

--- lib/sidekiq-rate-limiter/server.rb
+++ lib/sidekiq-rate-limiter/server.rb
@@ -2,5 +2,12 @@ require 'sidekiq-rate-limiter/version'
 require 'sidekiq-rate-limiter/fetch'
 
 Sidekiq.configure_server do |config|
-  Sidekiq.options[:fetch] = Sidekiq::RateLimiter::Fetch
+#   Sidekiq.options[:fetch] = Sidekiq::RateLimiter::Fetch
+
+  Sidekiq.options[:fetch] =
+    if Gem::Version.new(Sidekiq::VERSION) < Gem::Version.new("6.1.0")
+      Sidekiq::RateLimiter::Fetch
+    else
+      Sidekiq::RateLimiter::Fetch.new(Sidekiq.options)
+    end
 end
